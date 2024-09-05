b1f51922aaf7bd0cc7cbbeeb402be367192fbbbe
Esranur Kumaş <kumasesra2191>
2024-09-05 19:22:30 +0300

    binding ile yazıldı

--- app/src/main/java/com/example/filmoneriuygulamasi/HomeFragment.kt
+++ app/src/main/java/com/example/filmoneriuygulamasi/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.example.filmoneriuygulamasi
 
-import MovieSuggestionsAdapter
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,36 +7,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.example.filmoneriuygulamasi.adapter.MovieSuggestionsAdapter
+import com.example.filmoneriuygulamasi.databinding.FragmentHomeBinding
 import com.example.filmoneriuygulamasi.network.ApiClient
 import com.example.filmoneriuygulamasi.network.MovieSuggestResponse
-
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class HomeFragment : Fragment() {
 
-    private lateinit var movieSuggestionsAdapter: MovieSuggestionsAdapter
-    private lateinit var recyclerViewMovieSuggestions: RecyclerView
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
+    private lateinit var movieSuggestionsAdapter: MovieSuggestionsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        // Binding başlatılır
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
 
-        // RecyclerView tanımları
-        recyclerViewMovieSuggestions = view.findViewById(R.id.recyclerViewMovieSuggestions)
+        // RecyclerView için GridLayoutManager ayarlanır
+        binding.recyclerViewMovieSuggestions.layoutManager = GridLayoutManager(context, 3)
 
-
-        // GridLayoutManager ve Adapter ayarları
-        recyclerViewMovieSuggestions.layoutManager = GridLayoutManager(context, 3)
-
-        // API'den veri yükleme fonksiyonları
+        // Film önerilerini API'den yükleme fonksiyonu çağrılır
         loadMovieSuggestions()
-
 
         return view
     }
@@ -54,7 +51,7 @@ class HomeFragment : Fragment() {
                     val movieSuggestions = response.body()?.result ?: emptyList()
                     Log.d("HomeFragment", "Movie Suggestions: $movieSuggestions")
                     movieSuggestionsAdapter = MovieSuggestionsAdapter(movieSuggestions)
-                    recyclerViewMovieSuggestions.adapter = movieSuggestionsAdapter
+                    binding.recyclerViewMovieSuggestions.adapter = movieSuggestionsAdapter
                 } else {
                     Log.e("HomeFragment", "Movie Suggestions Error: ${response.code()} - ${response.message()}")
                 }
@@ -66,5 +63,8 @@ class HomeFragment : Fragment() {
         })
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // Binding'i serbest bırak
+    }
 }
--- app/src/main/java/com/example/filmoneriuygulamasi/adapter/MovieSuggestionsAdapter.kt
+++ app/src/main/java/com/example/filmoneriuygulamasi/adapter/MovieSuggestionsAdapter.kt
@@ -1,49 +1,44 @@
+package com.example.filmoneriuygulamasi.adapter
+
 import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
-import com.example.filmoneriuygulamasi.R
-import com.example.filmoneriuygulamasi.network.MovieSuggestion
-import android.util.Log
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import com.example.filmoneriuygulamasi.R
+import com.example.filmoneriuygulamasi.databinding.ItemMovieSuggestionBinding
+import com.example.filmoneriuygulamasi.network.MovieSuggestion
 
 class MovieSuggestionsAdapter(
-    private val movieSuggestions: List<MovieSuggestion>
+    private val movieSuggestions: List<MovieSuggestion>,
 ) : RecyclerView.Adapter<MovieSuggestionsAdapter.MovieSuggestionViewHolder>() {
 
-    class MovieSuggestionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val posterImageView: ImageView = view.findViewById(R.id.imageViewPoster)
-        val titleTextView: TextView = view.findViewById(R.id.textViewTitle)
-        val durationTextView: TextView = view.findViewById(R.id.textViewDuration)
-        val genreTextView: TextView = view.findViewById(R.id.textViewGenre)
-    }
+    class MovieSuggestionViewHolder(val binding: ItemMovieSuggestionBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieSuggestionViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie_suggestion, parent, false)
-        return MovieSuggestionViewHolder(view)
+        val binding = ItemMovieSuggestionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieSuggestionViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MovieSuggestionViewHolder, position: Int) {
         val movieSuggestion = movieSuggestions[position]
         val movieLine = movieSuggestion.data.lines[0]
 
-        holder.titleTextView.text = movieLine.name
-        holder.durationTextView.text = movieLine.times
-        holder.genreTextView.text = movieLine.sty
+        holder.binding.textViewTitle.text = movieLine.name
+        holder.binding.textViewDuration.text = movieLine.times
+        holder.binding.textViewGenre.text = movieLine.sty
 
         // URL'yi HTTPS ile güncelle
         val secureUrl = movieLine.img.replace("http://", "https://")
 
         // Poster görselini Glide ile yükle
-        Glide.with(holder.posterImageView.context)
+        Glide.with(holder.binding.imageViewPoster.context)
             .load(secureUrl)
             .apply(RequestOptions()
                 .placeholder(R.drawable.ic_launcher_background) // Placeholder resmi
@@ -60,10 +55,8 @@ class MovieSuggestionsAdapter(
                     return false
                 }
             })
-            .into(holder.posterImageView)
+            .into(holder.binding.imageViewPoster)
     }
 
-    override fun getItemCount(): Int {
-        return movieSuggestions.size
-    }
+    override fun getItemCount(): Int = movieSuggestions.size
 }
